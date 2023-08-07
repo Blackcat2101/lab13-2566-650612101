fb9e8f2088418c9608b6a540997bc7bc96c667dc
santhod <santhod11111@gmail.com>
2023-08-07 16:23:38 +0700

    f2

--- src/app/search/[searchInput]/page.js
+++ src/app/search/[searchInput]/page.js
@@ -2,26 +2,20 @@
 
 import { MovieRow } from "@/components/MovieRow";
 import { movieDB } from "@/libs/movieDB";
-import { useEffect, useState } from "react";
 
 export default function SearchResultPage({ params }) {
   //tip1 : before filtering movie, replace all "%20" with " " (space) in the input
-  const searchInput = params.searchInput;
-  const processedSearchInput = searchInput.replaceAll("%20", " ");
-
-  const [filteredMovies, setFilteredMovies] = useState([]);
+  const processedSearchInput = params.searchInput.replaceAll("%20", " ");
 
   const filterMovies = () => {
     const lowerCaseSearchInput = processedSearchInput.toLocaleLowerCase();
     const filteredMovies = movieDB.filter((movie) =>
       movie.title.toLocaleLowerCase().includes(lowerCaseSearchInput)
     );
-    setFilteredMovies(filteredMovies);
+    return filteredMovies;
   };
 
-  useEffect(() => {
-    filterMovies();
-  }, []);
+  const filteredMovies = filterMovies();
 
   return (
     <div>
